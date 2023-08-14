ff7bda026b2d83988f278b19c8de5b6685ee064d
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2023-08-15 03:03:51 +0600

    First Commit

--- client_test.py
+++ client_test.py
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for n in quotes:
+      self.assertEqual(getDataPoint(n),{n['stock'],n['top_bid']['price'],n['top_ask']['price'],{n['top_bid']['price']+n['top_ask']['price']}/2})
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +18,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for n in quotes:
+      self.assertEqual(getDataPoint(n),{n['stock'],n['top_bid']['price'],n['top_ask']['price'],{n['top_bid']['price']+n['top_ask']['price']}/2})
 
 
   """ ------------ Add more unit tests ------------ """
-
 
 
 if __name__ == '__main__':
